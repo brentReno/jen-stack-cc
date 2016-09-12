56cd50ba88be1c79b69d0c84303b34f5d340764a
Brent Reno <brent.reno@gmail.com>
2016-09-12 14:27:34 -0500

    add on click for submit button, initiate handshake

--- public/scripts/jokes.js
+++ public/scripts/jokes.js
@@ -2,4 +2,7 @@ console.log("the jokes are sourced");
 
 $(document).ready(function(){
   console.log("JQ is ready to Rock n' Roll");
-});
+  $('#submit').on('click', function(){
+    console.log("in submit on click");
+  });//end submit on click
+});// end doc ready
