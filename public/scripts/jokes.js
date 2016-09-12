fead0f6785035480f0404a90ae3db59447c527a5
Brent Reno <brent.reno@gmail.com>
2016-09-12 14:32:06 -0500

    package joke data in object

--- public/scripts/jokes.js
+++ public/scripts/jokes.js
@@ -1,8 +1,14 @@
 console.log("the jokes are sourced");
-
+var joke;
 $(document).ready(function(){
   console.log("JQ is ready to Rock n' Roll");
   $('#submit').on('click', function(){
     console.log("in submit on click");
+    var author = $('#author').val();
+    var setup = $('#setup').val();
+    var punch = $("#punchline").val();
+    console.log("Joke info = ", author, setup, punch);
+    joke ={whoseJoke: author, jokeQuestion: setup, punchLine: punch};
+    console.log(joke);
   });//end submit on click
 });// end doc ready
