e45d3a73060b6f8904ec2ef2da9774274a32c15f
Brent Reno <brent.reno@gmail.com>
2016-09-12 14:22:05 -0500

    handshake with the server, and JS files

---
+++ public/scripts/jokes.js
@@ -0,0 +1,5 @@
+console.log("the jokes are sourced");
+
+$(document).ready(function(){
+  console.log("JQ is ready to Rock n' Roll");
+});
--- server/app.js
+++ server/app.js
@@ -16,3 +16,24 @@ jokes = [
     punchLine: "Too many Cheetahs"
   }
 ];
+var express = require('express');
+var app     = express();
+var port    =   process.env.PORT || 3000;
+var path = require( 'path' );
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded({ extended: false});
+
+//start listening
+app.listen( port, function(){
+  console.log('The server is listening on port 3000');
+});
+
+//set up base url
+app.get('/', function(req, res){
+  console.log("The base URL has been hit");
+  res.sendFile(path.resolve('public/index.html'));
+});//end base url
+
+//set public folder for use
+app.use(express.static('public'));
+app.use(urlEncodedParser);
